c6193b5d9e8f91c51455d4677930520cbc214adc
Pragmatic-Learner <nardevjuggoostudying@gmail.com>
2025-09-21 11:50:02 +0400

    to implement next: continuous looping and keyboard event detection

--- snake.c
+++ snake.c
@@ -1,18 +1,20 @@
-#define GRID 10
+#include <stdio.h>
+
+#define GRIDY 30
+#define GRIDX GRIDY * 2
 #define SPACE ' '
 #define HEAD '@'
 #define BODY '0'
-#define INITIALSIZE 5
+#define BODYVERTICAL '|'
+#define BODYHORIZONTAL '-'
+#define INITIALSIZE 3
 
-#include <stdio.h>
-#include <unistd.h>
 
-enum AXIS{X = 0, Y = 1};
+enum AXIS{X, Y, POINT};
 enum DIRECTION{RIGHT, LEFT, DOWN, UP, KILL};
 
-int snake[2][GRID * GRID];
-int length = INITIALSIZE;
-char key;
+int loop = 1;
+int snake[3][GRIDX * GRIDY], length = INITIALSIZE;
 
 void clr(){printf("\e[1;1H\e[2J");}
 void draw(), initializeSnake(), input_reader(), movement(int direction);
@@ -20,7 +22,7 @@ void draw(), initializeSnake(), input_reader(), movement(int direction);
 int main() {
   /*code gaes here*/
   initializeSnake();
-  while(1) {
+  while(loop) {
     clr();
     draw();
     input_reader();
@@ -33,41 +35,47 @@ void initializeSnake() {
   for (int initialize = 0; initialize < INITIALSIZE; initialize++) {
     snake[X][initialize] = INITIALSIZE - initialize - 1;
     snake[Y][initialize] = 0;
+    snake[POINT][initialize] = RIGHT;
   }
 }
 
 void draw(){
-  int found;
-  for (int x = 0; x < GRID; x++) {
+  for (int x = 0; x < GRIDX; x++)
     printf("_");
-  }
+
   printf("\n");
-  for (int y = 0; y < GRID; y++) {
-    for (int x = 0; x < GRID; x++) {
-      found = 0;
+
+  for (int y = 0; y < GRIDY; y++) {
+    for (int x = 0; x < GRIDX; x++) {
       for (int segment = 0; segment < length; segment++) {
+        /*Check each segment, must look for better way*/
         if (snake[X][segment] == x && snake[Y][segment] == y) {
-          printf("%c", BODY);
-          found = 1;
+          if (segment == 0) {
+            printf("%c", HEAD);
+          } else {
+            if (snake[POINT][segment] == LEFT || snake[POINT][segment] == RIGHT) {
+              printf("%c", BODYHORIZONTAL);
+            } else if (snake[POINT][segment] == UP || snake[POINT][segment] == DOWN) {
+              printf("%c", BODYVERTICAL);
+            }
+          }
           break;
+        } else if (segment == length - 1) {
+          printf("%c", SPACE);
         }
       }
-      if (found == 0) {
-        printf("%c", SPACE);
-      }
     }
-    printf("|\n");
-    //printf("\t| %d\n", y);
+    printf("|\t%d\n", y);
   }
-  for (int x = 0; x < GRID; x++) {
+
+  for (int x = 0; x < GRIDX; x++)
     printf("_");
-  }
+
   printf("\n");
 }
 
 void input_reader() {
-  scanf("%c", &key);
-  switch (key) {
+  switch (getchar()) {
     case 'a':
       movement(LEFT);
       break;
@@ -81,7 +89,9 @@ void input_reader() {
       movement(DOWN);
       break;
     case 'q':
-      movement(KILL);
+      loop = 0;
+    default:
+      break;
   }
 }
 
@@ -89,19 +99,24 @@ void movement(int direction) {
   for (int i = length - 1; i > 0; i--) {
     snake[X][i] = snake[X][i - 1];
     snake[Y][i] = snake[Y][i - 1];
+    snake[POINT][i] = snake[POINT][i - 1];
   }
   switch (direction) {
     case LEFT:
       snake[X][0] -= 1;
+      snake[POINT][0] = LEFT;
       break;
     case RIGHT:
       snake[X][0] += 1;
+      snake[POINT][0] = RIGHT;
       break;
     case UP:
       snake[Y][0] -= 1;
+      snake[POINT][0] = UP;
       break;
     case DOWN:
       snake[Y][0] += 1;
+      snake[POINT][0] = DOWN;
       break;
   }
 }
